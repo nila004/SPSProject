c8216212104ca100a4234a68a44fa7186590b51a
Nila <itsmenila2004@gmail.com>
2025-06-17 23:54:42 +0530

    committed changes

--- frontend/home.js
+++ frontend/home.js
@@ -1,24 +1,22 @@
-const { link } = require("fs");
-
 document.addEventListener('DOMContentLoaded', function () {
   const events = [
     {
       title: "Tech Symposium 2025",
-      description: "Showcase of student tech innovations across Keralaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaa aaaaaaaaa .",
+      description: "Showcase of student tech innovations across Keralaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaa.",
       image: "images/image1.jpg",
       link: "/login"
     },
     {
       title: "AI Bootcamp",
-      description: "Hands-on ML, Deep Learning and Generative AI workshop aaaa aaa aaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaa aaaaaaaaaaaaaaaa aaaaaaaaaaa.",
+      description: "Hands-on ML, Deep Learning and Generative AI workshop.",
       image: "images/image2.jpg",
-       link: "/login"
+      link: "/login"
     },
     {
       title: "RescueLink Demo Day",
       description: "Live trial of our event management dashboard system.",
       image: "images/image3.jpg",
-       link: "/login"
+      link: "/login"
     }
   ];
 
@@ -28,12 +26,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const card = document.createElement("div");
     card.className = "event-card";
 
-    // Limit preview to 150 characters
     const shortDescription = event.description.slice(0, 150);
     const needsToggle = event.description.length > 150;
 
     card.innerHTML = `
-      <img src="${event.image}" alt="${event.title}" class="event-image">
+      <img src="${event.image}" alt="${event.title}" class="event-image" style="cursor: pointer;">
       <div class="event-details">
         <div class="event-title">${event.title}</div>
         <div class="event-description">
